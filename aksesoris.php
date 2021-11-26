0c6a3368f6d80a1e47e3bc615d0c65aba346e966
anharID <63109799+anharID@users.noreply.github.com>
2021-11-26 17:30:22 +0700

    tampilan beranda untuk pembeli

---
+++ aksesoris.php
@@ -0,0 +1,70 @@
+<?php
+    include 'config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Xiomay Store</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- header-->
+    <header>
+        <div class="container">
+            <h1>Xiomay Store</h1>
+            <ul>
+                <li><a href="index.php">HP</a></li>
+                <li><a href="aksesoris.php">Aksesoris</a></li>
+                <li><a href="ekosistem.php">Ekosistem</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <!-- search -->
+    <div class="search">
+        <div class="container">
+            <form action="aksesoris.php">
+                <input type="text" name="search" placeholder="Cari Produk">
+                <input type="submit" name="cari" value="Cari Produk">
+            </form>
+        </div>
+    </div>
+
+    <div class="section">
+        <div class="container">
+            <h3>Daftar Aksesoris Tersedia</h3>
+            <div class="box">
+                <?php 
+                    
+					$aksesoris = mysqli_query($mysqli, "SELECT * FROM tb_aksesoris  ORDER BY id_aksesoris DESC");
+                    if(isset($_GET['search'])){
+                        $aksesoris = mysqli_query($mysqli, "SELECT * FROM tb_aksesoris WHERE nama LIKE '%".$_GET['search']."%' ORDER BY id_aksesoris DESC");
+                    }
+					if(mysqli_num_rows($aksesoris) > 0){
+						while($a = mysqli_fetch_array($aksesoris)){
+				?>
+                <a href="detail-beli.php?id=<?php echo $a['id_aksesoris'] ?>">
+                    <div class="col-4">
+                        <img src="assets/aksesoris/<?php echo $a['gambar']?>">
+                        <p class="nama"><?php echo $a['nama'] ?></p>
+                        <p class="harga">Rp. <?php echo number_format($a['harga']) ?></p>
+                    </div>
+                </a>
+                <?php }}else{ ?>
+                <p>Produk tidak ada</p>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+    include 'config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Xiomay Store</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- header-->
+    <header>
+        <div class="container">
+            <h1>Xiomay Store</h1>
+            <ul>
+                <li><a href="index.php">HP</a></li>
+                <li><a href="aksesoris.php">Aksesoris</a></li>
+                <li><a href="ekosistem.php">Ekosistem</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <!-- search -->
+    <div class="search">
+        <div class="container">
+            <form action="index.php" method="get">
+                <input type="text" name="keyword" placeholder="Cari Sesuatu">
+                <input type="submit" name="search" value="Cari Produk">
+            </form>
+        </div>
+    </div>
+
+    <div class="section">
+        <div class="container">
+            <h3>Daftar HP Tersedia</h3>
+            <div class="box">
+
+                <?php
+                        $hp = mysqli_query($mysqli, "SELECT * FROM tb_hp  ORDER BY id_hp DESC");
+                        if(isset($_GET['keyword'])){
+                            $hp = mysqli_query($mysqli, "SELECT * FROM tb_hp WHERE nama LIKE '%".$_GET['keyword']."%' ORDER BY id_hp DESC");
+                        }
+					    if(mysqli_num_rows($hp) > 0){
+						while($h = mysqli_fetch_array($hp)){
+                            
+				?>
+                <a href="detail-beli.php?id=<?php echo $h['id_hp'] ?>">
+                    <div class="col-4">
+                        <img src="assets/hp/<?php echo $h['gambar'] ?>">
+                        <p class="nama"><?php echo substr($h['nama'], 0, 30) ?></p>
+                        <p class="harga">Rp. <?php echo number_format($h['harga']) ?></p>
+                    </div>
+                </a>
+
+                <?php }}else{ ?>
+                <p>Produk tidak ada</p>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
